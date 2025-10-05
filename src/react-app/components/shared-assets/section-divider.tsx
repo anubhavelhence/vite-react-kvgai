09df21c3702db787a6f0bd5faae4f1d55765e256
Anubhav Elhence <elhenceanubhav@gmail.com>
2025-10-05 10:13:53 +0530

    unititled

---
+++ src/react-app/components/shared-assets/section-divider.tsx
@@ -0,0 +1,3 @@
+export const SectionDivider = () => {
+    return <div className="border-t border-secondary" />;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
+import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [
+    react(),
+    tailwindcss(),cloudflare()
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src/react-app'),
+    },
+  },
 });
